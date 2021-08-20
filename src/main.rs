ec1aa042b6ce5079ce720a532876911784180b73
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-08-20 23:14:56 +0900

    mian

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,48 @@ mod udp_client;
 mod udp_server;
 fn main() {
     env::set_var("RUST_LOG", "debug");
-    envlogger::init();
+    env_logger::init();
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
         error!("Please specify [tcp/udp] [server/client] [addr:port].");
         std::process::exit(1);
     }
+    let protocol: &str = &args[2];
+    let address = &args[3];
+    match protocol {
+        "tcp" => match role {
+            "server" => {
+                //todo: tcpサーバの呼び出し
+            }
+            "client" => {
+                //todo: call tcp client
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                //todo: tcpサーバの呼び出し
+            }
+            "client" => {
+                //todo: call tcp client
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        _ => {
+            error!("Please specify tcp or usp on the 1st argumet.");
+            std::process::exit(1);
+        }
+    }
+}
+
+/**
+ * 第二引数が不正だった場合に出す関数
+ */
+fn missing_role() {
+    error!("Please specify server or client on the 2st argumet.");
+    std::process::exit(1);
 }
