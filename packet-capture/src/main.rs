f6ea7c3ab15e68bea6142bb94429a71ef8544eb0
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-24 21:54:30 +0900

    manupulate socket

--- packet-capture/src/main.rs
+++ packet-capture/src/main.rs
@@ -29,4 +29,113 @@ fn main() {
         .into_iter()
         .find(|iface| iface.name == *interface_name)
         .expect("fale");
+
+    let (_tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => {
+            panic!("Failed to create datalink channel {}", e)
+        }
+    };
+
+    loop {
+        match rx.next() {
+            Ok(frame) => {
+                let frame = EthernetPacket::new(frame).unwrap();
+                match frame.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&frame);
+                    }
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&frame);
+                    }
+                    _ => {
+                        println!("Not a ipv4 or ipv6");
+                    }
+                }
+            }
+            Err(e) => {
+                panic!("Failed to read: {}", e);
+            }
+        }
+    }
+}
+
+fn ipv4_handler(ethernet: &EthernetPacket) {
+    if let Some(v) = Ipv4Packet::new(ethernet.payload()) {
+        match v.get_next_level_protocol() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&v);
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&v);
+            }
+            _ => {
+                println!("hogeho");
+            }
+        }
+    }
+}
+
+fn ipv6_handler(ethernet: &EthernetPacket) {
+    if let Some(v) = Ipv6Packet::new(ethernet.payload()) {
+        match v.get_next_header() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&v);
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&v);
+            }
+            _ => {
+                println!("hogehoge");
+            }
+        }
+    }
+}
+
+fn tcp_handler(packet: &GettableEndPoints) {
+    let tcp = TcpPacket::new(packet.get_payload());
+    if let Some(tcp) = tcp {
+        print_packet_info(packet, &tcp, "TCP");
+    }
+}
+
+fn udp_handler(packet: &GettableEndPoints) {
+    let udp = UdpPacket::new(packet.get_payload());
+    if let Some(udp) = udp {
+        print_packet_info(packet, &udp, "UDP");
+    }
+}
+
+fn print_packet_info(l3: &GettableEndPoints, l4: &GettableEndPoints, proto: &str) {
+    println!(
+        "Captured a {} packet from {}|{} to {}|{}\n",
+        proto,
+        l3.get_source(),
+        l4.get_source(),
+        l3.get_destination(),
+        l4.get_destination()
+    );
+    let payload = l4.get_payload();
+    let len = payload.len();
+
+    for i in 0..len {
+        print!("{:<02X} ", payload[i]);
+        if i % WIDTH == WIDTH - 1 || i == len - 1 {
+            for _j in 0..WIDTH - 1 - (i % (WIDTH)) {
+                print!("   ");
+            }
+            print!("| ");
+            for j in i - i % WIDTH..i + 1 {
+                if payload[j].is_ascii_alphabetic() {
+                    print!("{}", payload[j] as char);
+                } else {
+                    print!(".");
+                }
+            }
+            print!("\n");
+        }
+    }
+    println!("{}", "=".repeat(WIDTH * 3));
+    print!("\n");
 }
